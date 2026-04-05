2ed207155128e3fb54a604f62cd9d18e2577afd7
Tiago Lopes da Silva <Tiago_FLDS@Outlook.com>
2026-04-05 04:43:58 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-teste
+// ===== Inicialização =====
+document.addEventListener("DOMContentLoaded", () => {
+  preencherSelects();
+
+  document
+    .getElementById("btn-verificar")
+    .addEventListener("click", verificarCompatibilidades);
+});
