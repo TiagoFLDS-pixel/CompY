4bc81b6bcc5bd135e07aaf528a8c4ce9b7cf2c9a
Tiago Lopes da Silva <Tiago_FLDS@Outlook.com>
2026-04-17 00:33:25 +0100

    Add service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "compy-v1";
+
+const URLS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./favicon.png",
+  "./data/compatibilidades.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
