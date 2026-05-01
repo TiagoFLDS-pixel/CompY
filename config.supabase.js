2ecb4bbeb0885e86a4d84a1c6fdca2755175cd56
Tiago Lopes da Silva <Tiago_FLDS@Outlook.com>
2026-05-01 19:12:47 +0100

    Add Supabase configuration file
    Initialize Supabase configuration with URL and anon key.

---
+++ config.supabase.js
@@ -0,0 +1,10 @@
+// config.supabase.js
+
+const SUPABASE_URL = "https://uyuutsygidbjlwmurkfl.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV5dXV0c3lnaWRiamx3bXVya2ZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcyNzkzMDksImV4cCI6MjA5Mjg1NTMwOX0.i8mm3C70x221lNyEE_fFPsLwTkDvvA0D6aS3nvZMqh4";
+
+// Cliente Supabase usado pela app CompY
+window.compYSupabase = window.supabase.createClient(
+  window.SUPABASE_URL,
+  window.SUPABASE_ANON_KEY
+);
