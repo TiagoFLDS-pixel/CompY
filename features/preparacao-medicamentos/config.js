6ba98d52bf6966e9a4c11199e5c4d0042a0f1fd8
Tiago Lopes da Silva <Tiago_FLDS@Outlook.com>
2026-09-23 06:46:15 +0100

    Enable PREPARACAO_MEDICAMENTOS feature flag
    Add experimental feature flag for medication preparation module

---
+++ features/preparacao-medicamentos/config.js
@@ -0,0 +1,6 @@
+window.COMPY_FEATURES = Object.freeze({
+  ...(window.COMPY_FEATURES || {}),
+
+  // Módulo visível em modo experimental; conteúdo clínico continua por validar.
+  PREPARACAO_MEDICAMENTOS: true
+});
